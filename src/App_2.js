40b78ebb29212d461b3d5dbde6dbd3a626edba6e
tothricsaj <tothricsaj@gmail.com>
2019-11-18 16:25:21 +0100

    :zap:

---
+++ src/App_2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App_2(props) {
+    return (
+        <div className="App">
+          <header className="App-header">
+              <h2>I am learning</h2>
+              <h2>Wtf</h2>
+          </header>
+        </div>
+  );
+}
+
+export default App_2;
