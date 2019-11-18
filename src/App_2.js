7a51d126b5e210bf60695cb8872e2659b55c8744
tothricsaj <tothricsaj@gmail.com>
2019-11-18 16:37:47 +0100

    basic staff :robot:

--- src/App_2.js
+++ src/App_2.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-function App_2(props) {
-    return (
+class App_2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({msg: e.target.value});
+    }
+
+    render() {
+        let pStyle = {
+            borderBottom: '1px solid white',
+            position: 'absolute',
+            top: '50px',
+            left: '50px'
+        }
+        return (
         <div className="App">
           <header className="App-header">
+              <p className="shownText" style={pStyle}>{this.state.msg}</p>
               <h2>I am learning</h2>
-              <h2>Wtf</h2>
+              <input type="text" value={this.state.msg} onChange={this.handleChange} />
           </header>
         </div>
-  );
+      );
+    }
 }
 
 export default App_2;
